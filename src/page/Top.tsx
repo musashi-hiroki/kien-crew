be0d4f53a9ee598ee7f6cb6847f8308c2eee7628
hiroki_satou <satohiroki1016@gmail.com>
2024-03-08 13:34:10 +0900

    topにprofileを追加

--- src/page/Top.tsx
+++ src/page/Top.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import WhatsUp from "./WhatsUs";
+import Profile from "./Profile";
 
 export default function Top(){
     const [width, setWidth] = useState<number>(document.body.clientWidth)
@@ -14,7 +15,8 @@ export default function Top(){
     return(
         <>
             <img src="/src/assets/kien_top.JPG" width={width} className="fade-in mb-5"/>
-            <WhatsUp></WhatsUp>
+            <WhatsUp/>
+            <Profile/>
         </>
     )
 
